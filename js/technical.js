a094ec0b0292147ac0ac6453d8090a3d793deb8a
Aryan Kumar <aryanak9163@gmail.com>
2024-01-25 02:23:17 +0530

    Remove unnecessary files and update heading in index.html and added categories

---
+++ js/technical.js
@@ -0,0 +1,33 @@
+console.log('technical.js loaded');
+
+document.querySelector("#cardContainer1").addEventListener("click", function () {
+    document.querySelector("#card1").classList.toggle("flipped");
+});
+
+document.querySelector("#cardContainer2").addEventListener("click", function () {
+    document.querySelector("#card2").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer3").addEventListener("click", function () {
+    document.querySelector("#card3").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer4").addEventListener("click", function () {
+    document.querySelector("#card4").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer5").addEventListener("click", function () {
+    document.querySelector("#card5").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer6").addEventListener("click", function () {
+    document.querySelector("#card6").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer7").addEventListener("click", function () {
+    document.querySelector("#card7").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer8").addEventListener("click", function () {
+    document.querySelector("#card8").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer9").addEventListener("click", function () {
+    document.querySelector("#card9").classList.toggle("flipped");
+});
+document.querySelector("#cardContainer10").addEventListener("click", function () {
+    document.querySelector("#card10").classList.toggle("flipped");
+});
